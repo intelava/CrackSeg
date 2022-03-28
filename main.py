527d633bbed1222e9cbf47aeb6b0c0160c5e6166
Tarkan Atak Kan <70338971+intelava@users.noreply.github.com>
2022-03-29 01:03:53 +0300

    something promising

--- main.py
+++ main.py
@@ -1,234 +1,208 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from datetime import datetime
 import tensorflow as tf
-import cv2
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import pyplot as plt
-
-
-
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
-from tensorflow.keras.layers import AveragePooling2D, GlobalAveragePooling2D, UpSampling2D, Reshape, Dense
-from tensorflow.keras.models import Model
-from tensorflow.keras.applications import ResNet50
-from glob import glob
-from tensorflow.keras.optimizers import Adam
-
-
-import scipy
+import cv2
 import os
+from glob import glob
+print("ho")
 
-import keras
-
-from PIL import Image, ImageOps
-from skimage import transform
-#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-
-def load(filename):
-   np_image = Image.open(filename)
-   np_image = np.array(np_image).astype('float32')/255
-   np_image = transform.resize(np_image, (224, 224, 3))
-   np_image = np.expand_dims(np_image, axis=0)
-   return np_image
-
-
-def iou(y_true, y_pred):
-   def f(y_true, y_pred):
-      intersection = (y_true * y_pred).sum()
-      union = y_true.sum() + y_pred.sum() - intersection
-      x = (intersection + 1e-15) / (union + 1e-15)
-      x = x.astype(np.float32)
-      return x
-
-   return tf.numpy_function(f, [y_true, y_pred], tf.float32)
-
-
-smooth = 1e-15
-
-
-def dice_coef(y_true, y_pred):
-   new = y_true.numpy()
-
-
-
-   # Adds a subplot at the 1st position
-   fig = plt.figure()
-   fig.add_subplot(1, 2, 1)
-
-   # showing image
-   plt.imshow(new[0,:,:,0])
-   plt.axis('off')
-   plt.title("y_true")
-
-   # Adds a subplot at the 2nd position
-   fig.add_subplot(1, 2, 2)
-
-   # showing image
-   plt.imshow(y_pred[0,:,:])
-   plt.axis('off')
-   plt.title("pred")
-   plt.show()
-   y_true = tf.keras.layers.Flatten()(y_true)
-   y_pred = tf.keras.layers.Flatten()(y_pred)
-   intersection = tf.reduce_sum(y_true * y_pred)
-   return (2. * intersection + smooth) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth)
-
-
-def dice_loss(y_true, y_pred):
-   return 1.0 - dice_coef(y_true, y_pred)
+train_path = 'Btrain'
+val_path = 'Btest'
 
 
 
 
-def loadCV(image, mask):
-   image = image.decode()
-   mask = mask.decode()
-   image = cv2.imread(image, cv2.IMREAD_COLOR)
-   image = image.astype(np.float32)
-   #image = cv2.resize(image,(160,160))
 
-   mask = cv2.imread(mask,cv2.IMREAD_GRAYSCALE)
-   mask = mask.astype(np.float32)
-   #mask = cv2.resize(mask, (160, 160))
-   mask = np.expand_dims(mask, axis=-1)
-   return image,mask
+def collect(path):
+    images = []
+    masks = []
+    a = 0
+    for filename in os.listdir(path + '/images'):
+        a += 1
+        image = cv2.imread(path + '/images/' + filename, cv2.IMREAD_COLOR)
+        image = cv2.resize(image, (128, 128))
+        image = np.expand_dims(image, axis=0)
+        images.append(image)
+        if a == 400:
+            break
+    a = 0
+    for filename in os.listdir(path + '/masks'):
+        a += 1
+        mask = cv2.imread(path + '/masks/' + filename, cv2.IMREAD_GRAYSCALE)
+        mask = cv2.resize(mask, (128, 128))
+        mask = mask / 255.0  # Making mask pixels between 0 and 1
+        masks.append(mask)
+        if a == 400:
+            break
 
-def load2(X,Y):
-   newx = load(X)
-   newy = load(Y)
-   return newx, newy
+    #image_paths = sorted(glob(str(os.path.join(path, "images", "*jpg"))))
+    #mask_paths = sorted(glob(str(os.path.join(path, 'masks', '*jpg'))))
 
-def create_set(X,Y):
-   dataset = tf.data.Dataset.from_tensor_slices((X,Y))
-   dataset = dataset.map(tf_parse)
-   dataset = dataset.batch(2)
-   dataset = dataset.prefetch(10)
-   return dataset
+    return images, masks
 
+def load(path):
+    image_paths, mask_paths = collect(path)
 
-def tf_parse(x, y):
+    #image, mask = read(path + '/images', path + '/masks')
+    dataset = tf.data.Dataset.from_tensor_slices((image_paths,mask_paths))
+    dataset.batch(1)
+    dataset.prefetch(4)
+    return dataset
 
-   x, y = tf.numpy_function(loadCV, [x, y], [tf.float32, tf.float32])
-   x.set_shape([448, 448, 3])
-   y.set_shape([448, 448, 1])
-   return x, y
+train_crack = load('Btrain')
+test_crack = load('Btest')
 
+def resize(input_image, input_mask):
+   input_image = tf.image.resize(input_image, (128, 128), method="nearest")
+   input_mask = tf.image.resize(input_mask, (128, 128), method="nearest")
 
-def SqueezeAndExcite(inputs, ratio=8):
-   init = inputs
-   filters = init.shape[-1]
-   se_shape = (1, 1, filters)
+   return input_image, input_mask
 
-   se = GlobalAveragePooling2D()(init)
-   se = Reshape(se_shape)(se)
-   se = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
-   se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
-   x = init * se
+def augment(input_image, input_mask):
+   if tf.random.uniform(()) > 0.5:
+       # Random flipping of the image and mask
+       input_image = tf.image.flip_left_right(input_image)
+       input_mask = tf.image.flip_left_right(input_mask)
+
+   return input_image, input_mask
+
+def normalize(input_image, input_mask):
+   input_image = tf.cast(input_image, tf.float32) / 255.0
+   input_mask -= 1
+   return input_image, input_mask
+
+def load_image_train(datapoint):
+   input_image = datapoint["image"]
+   input_mask = datapoint["segmentation_mask"]
+   input_image, input_mask = resize(input_image, input_mask)
+   input_image, input_mask = augment(input_image, input_mask)
+   input_image, input_mask = normalize(input_image, input_mask)
+
+   return input_image, input_mask
+
+def load_image_test(datapoint):
+   input_image = datapoint["image"]
+   input_mask = datapoint["segmentation_mask"]
+   input_image, input_mask = resize(input_image, input_mask)
+   input_image, input_mask = normalize(input_image, input_mask)
+
+   return input_image, input_mask
+
+#train_dataset = dataset["train"].map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
+#test_dataset = dataset["test"].map(load_image_test, num_parallel_calls=tf.data.AUTOTUNE)
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+#train_batches = train_dataset.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+#train_batches = train_batches.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+#validation_batches = test_dataset.take(3000).batch(BATCH_SIZE)
+#test_batches = test_dataset.skip(3000).take(669).batch(BATCH_SIZE)
+
+def display(display_list):
+ plt.figure(figsize=(15, 15))
+
+ title = ["Input Image", "True Mask", "Predicted Mask"]
+
+ for i in range(len(display_list)):
+   plt.subplot(1, len(display_list), i+1)
+   plt.title(title[i])
+   plt.imshow(tf.keras.utils.array_to_img(display_list[i]))
+   plt.axis("off")
+ plt.show()
+
+#sample_batch = next(iter(train_batches))
+#random_index = np.random.choice(sample_batch[0].shape[0])
+#sample_image, sample_mask = sample_batch[0][random_index], sample_batch[1][random_index]
+#display([sample_image, sample_mask])
+
+def double_conv_block(x, n_filters):
+
+   # Conv2D then ReLU activation
+   x = layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
+   # Conv2D then ReLU activation
+   x = layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
+
    return x
 
+def downsample_block(x, n_filters):
+   f = double_conv_block(x, n_filters)
+   p = layers.MaxPool2D(2)(f)
+   p = layers.Dropout(0.3)(p)
 
-def ASPP(inputs):
-   """ Image Pooling """
-   shape = inputs.shape
-   y1 = AveragePooling2D(pool_size=(shape[1], shape[2]))(inputs)
-   y1 = Conv2D(256, 1, padding="same", use_bias=False)(y1)
-   y1 = BatchNormalization()(y1)
-   y1 = Activation("relu")(y1)
-   y1 = UpSampling2D((shape[1], shape[2]), interpolation="bilinear")(y1)
+   return f, p
 
-   """ 1x1 conv """
-   y2 = Conv2D(256, 1, padding="same", use_bias=False)(inputs)
-   y2 = BatchNormalization()(y2)
-   y2 = Activation("relu")(y2)
+def upsample_block(x, conv_features, n_filters):
+   # upsample
+   x = layers.Conv2DTranspose(n_filters, 3, 2, padding="same")(x)
+   # concatenate
+   x = layers.concatenate([x, conv_features])
+   # dropout
+   x = layers.Dropout(0.3)(x)
+   # Conv2D twice with ReLU activation
+   x = double_conv_block(x, n_filters)
 
-   """ 3x3 conv rate=6 """
-   y3 = Conv2D(256, 3, padding="same", use_bias=False, dilation_rate=6)(inputs)
-   y3 = BatchNormalization()(y3)
-   y3 = Activation("relu")(y3)
+   return x
 
-   """ 3x3 conv rate=12 """
-   y4 = Conv2D(256, 3, padding="same", use_bias=False, dilation_rate=12)(inputs)
-   y4 = BatchNormalization()(y4)
-   y4 = Activation("relu")(y4)
+def build_unet_model():
+ # inputs
+   inputs = layers.Input(shape=(128,128,3))
 
-   """ 3x3 conv rate=18 """
-   y5 = Conv2D(256, 3, padding="same", use_bias=False, dilation_rate=18)(inputs)
-   y5 = BatchNormalization()(y5)
-   y5 = Activation("relu")(y5)
+   # encoder: contracting path - downsample
+   # 1 - downsample
+   f1, p1 = downsample_block(inputs, 64)
+   # 2 - downsample
+   f2, p2 = downsample_block(p1, 128)
+   # 3 - downsample
+   f3, p3 = downsample_block(p2, 256)
+   # 4 - downsample
+   f4, p4 = downsample_block(p3, 512)
 
-   y = Concatenate()([y1, y2, y3, y4, y5])
-   y = Conv2D(256, 1, padding="same", use_bias=False)(y)
-   y = BatchNormalization()(y)
-   y = Activation("relu")(y)
+   # 5 - bottleneck
+   bottleneck = double_conv_block(p4, 1024)
 
-   return y
+   # decoder: expanding path - upsample
+   # 6 - upsample
+   u6 = upsample_block(bottleneck, f4, 512)
+   # 7 - upsample
+   u7 = upsample_block(u6, f3, 256)
+   # 8 - upsample
+   u8 = upsample_block(u7, f2, 128)
+   # 9 - upsample
+   u9 = upsample_block(u8, f1, 64)
 
-def deeplabv3_plus(shape):
-   """ Input """
-   inputs = Input(shape)
+   # outputs
+   outputs = layers.Conv2D(3, 1, padding="same", activation = "softmax")(u9)
 
-   """ Encoder """
-   encoder = ResNet50(weights="imagenet", include_top=False, input_tensor=inputs)
+   # unet model with Keras Functional API
+   unet_model = tf.keras.Model(inputs, outputs, name="U-Net")
 
-   image_features = encoder.get_layer("conv4_block6_out").output
-   x_a = ASPP(image_features)
-   x_a = UpSampling2D((4, 4), interpolation="bilinear")(x_a)
+   return unet_model
+print("ho")
+unet_model = build_unet_model()
+print("ho")
+unet_model.summary()
 
-   x_b = encoder.get_layer("conv2_block2_out").output
-   x_b = Conv2D(filters=48, kernel_size=1, padding='same', use_bias=False)(x_b)
-   x_b = BatchNormalization()(x_b)
-   x_b = Activation('relu')(x_b)
+unet_model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss="sparse_categorical_crossentropy",
+                  metrics="accuracy")
 
-   x = Concatenate()([x_a, x_b])
-   x = SqueezeAndExcite(x)
+NUM_EPOCHS = 20
 
-   x = Conv2D(filters=256, kernel_size=3, padding='same', use_bias=False)(x)
-   x = BatchNormalization()(x)
-   x = Activation('relu')(x)
+#TRAIN_LENGTH = info.splits["train"].num_examples
+#STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
 
-   x = Conv2D(filters=256, kernel_size=3, padding='same', use_bias=False)(x)
-   x = BatchNormalization()(x)
-   x = Activation('relu')(x)
-   x = SqueezeAndExcite(x)
-
-   x = UpSampling2D((4, 4), interpolation="bilinear")(x)
-   x = Conv2D(1, 1)(x)
-   x = Activation("sigmoid")(x)
-
-   model = Model(inputs, x)
-   return model
-
-
-#test ve train klasörlerini aşağı yazın 
-images = sorted(glob(os.path.join("Btrain/images", "*jpg")))
-maskes = sorted(glob(os.path.join("Btrain/masks", "*jpg")))
-
-images2 = sorted(glob(os.path.join("Btest/images", "*jpg")))
-masks2 = sorted(glob(os.path.join("Btest/masks", "*jpg")))
-
-images = images[100:200]
-masks = maskes[100:200]
+VAL_SUBSPLITS = 5
+#TEST_LENTH = info.splits["test"].num_examples
+#VALIDATION_STEPS = TEST_LENTH // BATCH_SIZE // VAL_SUBSPLITS
 
 
 
 
 
-dataset = create_set(images,masks)
 
-datasetVal = create_set(images2,masks2)
-
-
-
-
-model = deeplabv3_plus((448,448,3))
-
-
-model.compile(optimizer="adam",loss=dice_loss,metrics=[dice_coef, iou,'accuracy'],run_eagerly=True)
-
-model.fit(dataset,epochs=3,validation_data=datasetVal)
-
-model.save('')
+model_history = unet_model.fit(train_crack,
+                              epochs=NUM_EPOCHS,
+                              validation_data=test_crack)
